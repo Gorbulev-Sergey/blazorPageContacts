63f36878324f5623705b3adddc789a8fc4a0cc8b
Серёжа <gorbulevsv@yandex.ru>
2021-04-06 22:48:26 +0300

    Добавьте файлы проекта.

---
+++ Models/contacts.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace blazorPageContacts.Models
+{
+    public class contacts
+    {
+        public Dictionary<string, string> schedule { get; set; } = new Dictionary<string, string>();
+        public Dictionary<string, string> phones { get; set; } = new Dictionary<string, string>();
+        public Dictionary<string, string> socials { get; set; } = new Dictionary<string, string>();
+        public KeyValuePair<string, string> map { get; set; } = new KeyValuePair<string, string>();
+    }
+}
---
+++ Services/IContactsService.cs
@@ -0,0 +1,71 @@
+﻿using blazorPageContacts.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace blazorPageContacts.Services
+{
+    public interface IContactsService
+    {
+        contacts contacts { get; set; }
+        void addScheduleString(KeyValuePair<string, string> pare);
+        void addPhonesString(KeyValuePair<string, string> pare);
+        void addSocialsString(KeyValuePair<string, string> pare);
+        void replaceMap(KeyValuePair<string, string> pare);
+    }
+
+    public class ContactsService : IContactsService
+    {
+        public contacts contacts
+        {
+            get
+            {
+                using (StreamReader reader = new StreamReader(new FileStream(path: "contacts.json", mode: FileMode.OpenOrCreate)))
+                {
+                    return JsonConvert.DeserializeObject<contacts>(reader.ReadToEnd());
+                }
+            }
+            set
+            {
+                using (StreamWriter writer = new StreamWriter(new FileStream(path: "contacts.json", mode: FileMode.Truncate)))
+                {
+                    writer.Write(JsonConvert.SerializeObject(value));
+                }
+            }
+            
+        }
+        public void addScheduleString(KeyValuePair<string, string> pare)
+        {
+            var new_contacts = contacts;
+            new_contacts.schedule.Add(pare.Key, pare.Value);
+            contacts = new_contacts;
+        }
+
+        public void addPhonesString(KeyValuePair<string, string> pare)
+        {
+            var new_contacts = contacts;
+            new_contacts.phones.Add(pare.Key, pare.Value);
+            contacts = new_contacts;
+        }
+
+        public void addSocialsString(KeyValuePair<string, string> pare)
+        {
+            var new_contacts = contacts;
+            new_contacts.socials.Add(pare.Key, pare.Value);
+            contacts = new_contacts;
+        }
+
+        public void replaceMap(KeyValuePair<string, string> pare)
+        {
+            var new_contacts = contacts;
+            new_contacts.map = pare;
+            contacts = new_contacts;
+        }
+
+        
+    }
+}
